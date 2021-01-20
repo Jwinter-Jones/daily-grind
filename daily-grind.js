77e4e8e8b45b691e9e5d7f0b8064629eb7ce04fd
Jwinter-Jones <mr.goodypants@gmail.com>
2021-01-20 15:56:11 -0800

    added switch template

--- daily-grind.js
+++ daily-grind.js
@@ -1,1 +1,24 @@
+/*
+Here is the info we will need to change
+Image src
+alt tag
+Day of week
+Name of coffee
+Color
+Description
+
+*/
+
+ley myDate = new Date();
+let myDay = myDate.getDay();
+
+switch (myDay) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
 alert("Hi I'm JS!");
