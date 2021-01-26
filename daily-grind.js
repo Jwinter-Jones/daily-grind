66b398e410714e821d1c4ff8045f27fe370b03c8
Jwinter-Jones <mr.goodypants@gmail.com>
2021-01-25 17:14:31 -0800

    incomplete, added color options and test day cases

--- daily-grind.js
+++ daily-grind.js
@@ -11,14 +11,65 @@ Description
 
 let myDate = new Date();
 let myDay = myDate.getDay();
+let today = "";
+let coffee = "";
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 
 switch (myDay) {
-    case value:
-        
+    case 1:
+        today = "Monday";
+        coffee = {
+            color:"pink",
+            name:"Bubble Tea",
+            pic:"images/bubble-tea.jpg",
+            alt:"A picture of a Bubble Tea",
+            day: "Monday",
+            desc: `Sometimes you just need the drip`
+        };
+        break;
+
+    case 3:
+        today = "Wednesday";
+        coffee = {
+            color:"brown",
+            name:"Drip",
+            pic:"images/drip.jpg",
+            alt:"A picture of a Drip Coffee",
+            day: "Wednesday",
+            desc: `Sometimes you just need the drip`
+        };
         break;
 
     default:
         break;
 }
+console.log(coffee);
 
-alert("Hi I'm JS!");
+if(urlParams.has('day')){//use querystring data
+    myDay = urlParams.get('day');
+}
+else{//use date object
+    myDay = myDate.getDay();
+}
+
+myDay = parseInt(myDay);
+
+function coffeeTemplate(coffee){
+    let myReturn = "";
+    myReturn += `
+    <p>
+    <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee">
+    <strong id="coffee-highlight" class="feature">${coffee.day}'s Coffee Special is:</strong>
+    <p>`;
+    console.log(myReturn);
+    return myReturn;
+
+}
+document.getElementById("coffee-output").innerHTML = coffeeTemplate(coffee);
+
+document.getElementsByTagName("HTML")[0].style.backgroundColor = coffee.color;
+
+document.getElementById("coffee-highlight").style.color = coffee.color;
+
+//console.log(today);
